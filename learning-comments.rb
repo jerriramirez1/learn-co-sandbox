82df9799c7294a4f6402612fb3878488de675a15
JERRI Ramirez <jerri.lynch@yahoo.com>
2020-02-15 05:12:58 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,13 @@
+ruby learning-comments.ruby
+# dont do this :
+puts (1#+1) 
+# Perform a constant expression evaluation 
+3
+# Assign constant 3 to bare-word variable triangle_sides 
+'triangle_sides'
+# from the three Dog Night song: "Joy to the world (jeremiahwas a Bullfrog)"
+puts "Joy to the world"
+puts "all the boys and girls"
+# puts "Joy to the fishes in the deep blue sea" 
+puts "Joy to you and me"  
+
